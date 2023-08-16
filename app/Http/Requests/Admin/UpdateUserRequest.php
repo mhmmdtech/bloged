f5c4f41f70dbf43cf5d67d7598cf69b65cc54c38
mhmmdtech <mhmmdtech@yahoo.com>
2023-08-16 14:53:27 +0330

    refactor: make using of namespaces more simple in requests and factories

--- app/Http/Requests/Admin/UpdateUserRequest.php
+++ app/Http/Requests/Admin/UpdateUserRequest.php
@@ -5,10 +5,7 @@ namespace App\Http\Requests\Admin;
 use App\Enums\GenderStatus;
 use App\Enums\MilitaryStatus;
 use App\Models\User;
-use App\Rules\AlphaSpace;
-use App\Rules\DontStartWithNumbers;
-use App\Rules\IrMobileNumber;
-use App\Rules\IrNationalCode;
+use App\Rules as CustomRule;
 use Illuminate\Support\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules;
@@ -33,13 +30,13 @@ class UpdateUserRequest extends FormRequest
     {
         $user = $this->route('user');
         return [
-            'first_name' => ['required', 'string', new AlphaSpace, 'min:2', 'max:100'],
-            'last_name' => ['required', 'string', new AlphaSpace, 'min:2', 'max:100'],
-            'national_code' => ['required', 'numeric', 'digits_between:5,100', new IrNationalCode, Rule::unique(User::class)->ignore($user)],
-            'mobile_number' => ['required', 'string', 'min:5', 'max:100', new IrMobileNumber],
+            'first_name' => ['required', 'string', new CustomRule\AlphaSpace, 'min:2', 'max:100'],
+            'last_name' => ['required', 'string', new CustomRule\AlphaSpace, 'min:2', 'max:100'],
+            'national_code' => ['required', 'numeric', 'digits_between:5,100', new CustomRule\IrNationalCode, Rule::unique(User::class)->ignore($user)],
+            'mobile_number' => ['required', 'string', 'min:5', 'max:100', new CustomRule\IrMobileNumber],
             'gender' => ['required', new Rules\Enum(GenderStatus::class)],
             'email' => ['required', 'string', 'email', 'min:5', 'max:255', Rule::unique(User::class)->ignore($user)],
-            'username' => ['required', 'string', 'alpha_num:ascii', 'min:2', 'max:100', new DontStartWithNumbers, Rule::unique(User::class)->ignore($user)],
+            'username' => ['required', 'string', 'alpha_num:ascii', 'min:2', 'max:100', new CustomRule\DontStartWithNumbers, Rule::unique(User::class)->ignore($user)],
             'avatar' => ['nullable', Rules\File::image()->max(200)],
             'birthday' => ['nullable', 'date', 'before_or_equal:' . Carbon::now()->subYears(10)->toDateString()],
             'military_status' => ['nullable', 'required_if:gender,' . GenderStatus::Male->value, new Rules\Enum(MilitaryStatus::class)],
--- app/Http/Requests/Auth/RegisterationRequest.php
+++ app/Http/Requests/Auth/RegisterationRequest.php
@@ -2,14 +2,9 @@
 
 namespace App\Http\Requests\Auth;
 
-use App\Enums\GenderStatus;
-use App\Enums\MilitaryStatus;
 use App\Models\User;
-use App\Rules\AlphaSpace;
-use App\Rules\DontStartWithNumbers;
-use App\Rules\IrMobileNumber;
-use App\Rules\IrNationalCode;
-use App\Rules\CaptchaValidator;
+use App\Enums;
+use App\Rules as CustomRule;
 use Illuminate\Support\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules;
@@ -34,20 +29,20 @@ class RegisterationRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'first_name' => ['required', 'string', new AlphaSpace, 'min:2', 'max:100'],
-            'last_name' => ['required', 'string', new AlphaSpace, 'min:2', 'max:100'],
-            'national_code' => ['required', 'numeric', 'digits_between:5,100', new IrNationalCode, Rule::unique(User::class)],
-            'mobile_number' => ['required', 'string', 'min:5', 'max:100', new IrMobileNumber],
-            'gender' => ['required', new Rules\Enum(GenderStatus::class)],
+            'first_name' => ['required', 'string', new CustomRule\AlphaSpace, 'min:2', 'max:100'],
+            'last_name' => ['required', 'string', new CustomRule\AlphaSpace, 'min:2', 'max:100'],
+            'national_code' => ['required', 'numeric', 'digits_between:5,100', new CustomRule\IrNationalCode, Rule::unique(User::class)],
+            'mobile_number' => ['required', 'string', 'min:5', 'max:100', new CustomRule\IrMobileNumber],
+            'gender' => ['required', new Rules\Enum(Enums\GenderStatus::class)],
             'email' => ['required', 'string', 'email', 'min:5', 'max:255', Rule::unique(User::class)],
-            'username' => ['required', 'string', 'alpha_num:ascii', 'min:2', 'max:100', new DontStartWithNumbers, Rule::unique(User::class)],
+            'username' => ['required', 'string', 'alpha_num:ascii', 'min:2', 'max:100', new CustomRule\DontStartWithNumbers, Rule::unique(User::class)],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
             'avatar' => ['nullable', Rules\File::image()->max(200)],
             'birthday' => ['nullable', 'date', 'before_or_equal:' . Carbon::now()->subYears(10)->toDateString()],
-            'military_status' => ['nullable', 'required_if:gender,' . GenderStatus::Male->value, new Rules\Enum(MilitaryStatus::class)],
+            'military_status' => ['nullable', 'required_if:gender,' . Enums\GenderStatus::Male->value, new Rules\Enum(Enums\MilitaryStatus::class)],
             'province_id' => ['nullable', 'exists:provinces,id'],
             'city_id' => ['nullable', 'required_with:province_id', 'exists:cities,id'],
-            'captcha_code' => ['required', 'numeric', 'digits_between:6,8', new CaptchaValidator]
+            'captcha_code' => ['required', 'numeric', 'digits_between:6,8', new CustomRule\CaptchaValidator]
         ];
     }
 
