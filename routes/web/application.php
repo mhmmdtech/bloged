94e8638d1bc0de6a91c2abaecfdb39537d2d4206
mhmmdtech <mhmmdtech@yahoo.com>
2023-08-16 14:26:14 +0330

    refactor: make using of namespaces more simple in routes

--- routes/web/application.php
+++ routes/web/application.php
@@ -1,25 +1,22 @@
 <?php
 
-use App\Http\Controllers\Application\HomeController;
-use App\Http\Controllers\Application\PostController;
-use App\Http\Controllers\Application\CategoryController;
-use App\Http\Controllers\Application\SearchController;
+use App\Http\Controllers\Application;
 use Illuminate\Support\Facades\Route;
 
 Route::permanentRedirect('/home', '/');
 
 Route::name('application.')->group(function () {
-    Route::get('/', HomeController::class)->name('home');
+    Route::get('/', Application\HomeController::class)->name('home');
 
-    Route::get('/search', SearchController::class)->name('search');
+    Route::get('/search', Application\SearchController::class)->name('search');
 
     Route::prefix('posts')->name('posts.')->group(function () {
-        Route::get('/', [PostController::class, 'index'])->name('index');
-        Route::get('/{post}/{slug?}', [PostController::class, 'show'])->name('show');
+        Route::get('/', [Application\PostController::class, 'index'])->name('index');
+        Route::get('/{post}/{slug?}', [Application\PostController::class, 'show'])->name('show');
     });
 
     Route::prefix('categories')->name('categories.')->group(function () {
-        Route::get('/', [CategoryController::class, 'index'])->name('index');
-        Route::get('/{category}/{slug?}', [CategoryController::class, 'show'])->name('show');
+        Route::get('/', [Application\CategoryController::class, 'index'])->name('index');
+        Route::get('/{category}/{slug?}', [Application\CategoryController::class, 'show'])->name('show');
     });
 });
